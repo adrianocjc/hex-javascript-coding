7e3be1f45f0908a91d25c1bc3b871c82326dbabb
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-07-18 19:01:06 -0300

    Update 5-http.js

--- 0x0A-Node_JS_basic/5-http.js
+++ 0x0A-Node_JS_basic/5-http.js
@@ -1,24 +1,26 @@
-
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  const { url } = req;
-  if (url === '/') res.end('Hello Holberton School!');
-  else if (url === '/students') {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     countStudents(process.argv[2])
-      .then((message) => {
-        const response = `This is the list of our students\n${message}`;
+      .then((data) => {
+        let num = 0;
+        let response = '';
+        for (const [key, value] of Object.entries(data)) {
+          num += value.length;
+          response += `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+        }
+        response = `This is the list of our students\nNumber of students: ${num}\n${response.slice(0, -1)}`;
         res.end(response);
       })
-      .catch((err) => {
-        res.end(`${err.message}\n`);
+      .catch((error) => {
+        res.end(`This is the list of our students\n${error.message}`);
       });
+  // res.end(`This is the list of our students ${dict}`);
   }
 });
-
 app.listen(1245);
-
 module.exports = app;
