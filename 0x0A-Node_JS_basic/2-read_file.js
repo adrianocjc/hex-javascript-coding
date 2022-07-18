07919f07125a38663ed29f71619d1bb5a3a19cd7
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-07-18 18:57:36 -0300

    Update 2-read_file.js

--- 0x0A-Node_JS_basic/2-read_file.js
+++ 0x0A-Node_JS_basic/2-read_file.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
 
-module.exports = function countStudents(path) {
+function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const fields = {};
-    const students = data.split('\n').map((student) => student.split(','));
-    students.shift();
-
-    console.log(`Number of students: ${students.length}`);
-
-    students.forEach((student) => {
-      if (!fields[student[3]]) fields[student[3]] = [];
-      fields[student[3]].push(student[0]);
-    });
-    Object.keys(fields).forEach((key) => {
-      console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
-    });
-  } catch (err) {
+    const datatxt = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    const data = [];
+    for (const i of datatxt) {
+      if (i !== '') {
+        data.push(i.split(','));
+      }
+    }
+    const idxFN = data[0].indexOf('firstname');
+    const idxFD = data[0].indexOf('field');
+    const dict = {};
+    for (const line of data) {
+      if (data.indexOf(line) !== 0) {
+        if (line[idxFD] in dict) {
+          dict[line[idxFD]].push(line[idxFN]);
+        } else {
+          dict[line[idxFD]] = [];
+          dict[line[idxFD]].push(line[idxFN]);
+        }
+      }
+    }
+    console.log(`Number of students: ${data.length - 1}`);
+    for (const [key, value] of Object.entries(dict)) {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    }
+  } catch (e) {
     throw new Error('Cannot load the database');
   }
-};
+}
+module.exports = countStudents;
