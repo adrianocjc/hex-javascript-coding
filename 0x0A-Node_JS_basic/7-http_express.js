3957f02b77ac811ca86bf583322f167420427afd
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-07-18 19:04:56 -0300

    Update 7-http_express.js

--- 0x0A-Node_JS_basic/7-http_express.js
+++ 0x0A-Node_JS_basic/7-http_express.js
@@ -3,21 +3,24 @@ const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get('/students', async (req, res) => {
-  countStudents(process.argv[2])
-    .then((message) => {
-      const response = `This is the list of our students\n${message}`;
-      res.send(response);
-    })
-    .catch((err) => {
-      res.send(`${err.message}`);
-    });
-});
-
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((data) => {
+      let num = 0;
+      let response = '';
+      for (const [key, value] of Object.entries(data)) {
+        num += value.length;
+        response += `Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`;
+      }
+      response = `This is the list of our students\nNumber of students: ${num}\n${response.slice(0, -1)}`;
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
 app.listen(1245);
-
 module.exports = app;
