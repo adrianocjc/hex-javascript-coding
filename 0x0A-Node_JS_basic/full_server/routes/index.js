15a6dd6b36e8c67fa6269a6b2d84ef829287445c
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-07-18 19:27:33 -0300

    Update index.js

--- 0x0A-Node_JS_basic/full_server/routes/index.js
+++ 0x0A-Node_JS_basic/full_server/routes/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentsController');
+const { Router } = require("express");
+const AppController = require("../controllers/AppController");
+const StudentsController = require("../controllers/StudentsController");
 
-const app = express();
+const router = Router();
 
-app.get('/', ((request, response) => AppController.getHomepage(request, response)));
-app.get('/students', ((request, response) => StudentsController.getAllStudents(request, response)));
-app.get('/students/:major', ((request, response) => StudentsController.getAllStudentsByMajor(request, response)));
+router.get("/", AppController.getHome);
+router.get("/students", StudentsController.getAllStudents);
+router.get("/students/:major", StudentsController.getAllStudentsByMajor);
 
-module.exports = app;
+module.exports = router;
