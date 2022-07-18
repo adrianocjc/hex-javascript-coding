c349d3611d397a19f86742fbc5b99ed4630d194c
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-07-18 17:55:21 -0300

    Update 4-update_grade_by_city.js

--- 0x09-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x09-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.city === city)
-    .map((student) => {
+  if (Array.isArray(students)) {
+    return students.filter((student) => student.location === city).map((student) => {
       const grade = newGrades.find((grade) => grade.studentId === student.id);
-      return {
-        grade: grade ? grade.grade : 'N/A',
-        ...student,
-      };
+      return grade !== undefined ? { ...student, grade: grade.grade } : { ...student, grade: 'N/A' };
     });
+  }
+  return 0;
 }
