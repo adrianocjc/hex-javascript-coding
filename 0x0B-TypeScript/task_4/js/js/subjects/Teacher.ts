25d2839507c250ff29d06f0defdb9299d1d6c02a
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-07-30 16:40:15 -0300

    Create Teacher.ts

---
+++ 0x0B-TypeScript/task_4/js/js/subjects/Teacher.ts
@@ -0,0 +1,10 @@
+/// <reference path="Cpp.ts" />
+/// <reference path="Java.ts" />
+/// <reference path="React.ts" />
+/// <reference path="Subject.ts" />
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
+}
