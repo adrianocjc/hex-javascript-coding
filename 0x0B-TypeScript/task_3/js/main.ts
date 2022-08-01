a95212f08acc98649edf8022440bd9a30225ea9f
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-08-01 11:59:19 -0300

    Update main.ts

--- 0x0B-TypeScript/task_3/js/main.ts
+++ 0x0B-TypeScript/task_3/js/main.ts
@@ -1,21 +1,21 @@
-/// <reference path = './crud.d.ts' />
+/// <reference path="./crud.d.ts" />
 
-import { RowElement, RowID } from './interface';
-import * as CRUD from './crud';
-
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
 
 const row: RowElement = {
-  firstName: 'Guilaume',
+  firstName: 'Guillaume',
   lastName: 'Salva',
-}
-
-const newRowID: RowID = CRUD.insertRow(row);
-
-const updatedRow: RowElement = {
-	...row,
-	age: 23,
 };
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
 
+const obj = {firstName: 'Guillaume', lastName: 'Salva'};
+CRUD.insertRow(obj)
+
+const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 
-CRUD.deleteRow(newRowID);
+CRUD.deleteRow(125);
