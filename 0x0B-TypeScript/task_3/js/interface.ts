897e64abe37bc07110cf067dc7cbf7ee072a9cdd
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-08-01 11:49:00 -0300

    Update interface.ts

--- 0x0B-TypeScript/task_3/js/interface.ts
+++ 0x0B-TypeScript/task_3/js/interface.ts
@@ -1,12 +1,7 @@
-type RowID = number;
+export type RowID = number;
 
-interface RowElement {
-	firstName: string;
-	lastName: string;
-	age?: number;
-}
-
-export {
-	RowID,
-	RowElement,
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
 }
