facdfbdeb3243b11a157de89ffb781a78217b22b
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-08-30 09:51:49 -0300

    Update main.ts

--- 0x0B-TypeScript/task_4/js/main.ts
+++ 0x0B-TypeScript/task_4/js/main.ts
@@ -1,30 +1,29 @@
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
-/// <reference path="./subjects/Subject.ts" />
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
 
-
-const cpp: Subjects.Cpp = new Subjects.Cpp();
-const java: Subjects.Java = new Subjects.Java();
-const react: Subjects.React = new Subjects.React();
-
-const cTeacher: Subjects.TeacherInterface = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-  experienceTeachingC: 10
-};
+export const cTeacher: Subjects.TeacherInterface = {
+	firstName: 'Betty',
+	lastName: 'Frizzle',
+	experienceTeachingC: 10
+}
 
 console.log('C++');
-cpp.setTeacher(cTeacher);
+cpp.teacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
 console.log('Java');
-java.setTeacher(cTeacher);
+java.teacher = cTeacher;
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
 console.log('React');
-react.setTeacher(cTeacher);
+react.teacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
