005159f34d07628eb4d0f992934ad6fa47986ed9
Adriano Costa <79386876+adrianocjc@users.noreply.github.com>
2022-08-30 09:38:30 -0300

    Update main.ts

--- 0x0B-TypeScript/task_5/js/main.ts
+++ 0x0B-TypeScript/task_5/js/main.ts
@@ -1,19 +1,18 @@
-type UUID = number;
-
+//credit: number;
 export interface MajorCredits {
-  credits: number;
-  brand: UUID;
+		credits: number;
+		_brand: 'Major';
 }
 
 export interface MinorCredits {
-  credits: number;
-  brand: UUID;
+	credits: number;
+	_brand: 'Minor';
 }
 
-export function sumMajorCredits (subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credits: subject1.credits + subject2.credits } as MajorCredits
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+	return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-export function sumMinorCredits (subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credits: subject1.credits + subject2.credits } as MinorCredits
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+	return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
